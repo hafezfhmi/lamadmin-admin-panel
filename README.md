41acf1a422e6f8d1732d1865147aae4b17c42b0a
hafezfhmi <hafezfahmi2000@gmail.com>
2022-10-13 02:53:46 +0800

    Reupload

---
+++ README.md
@@ -0,0 +1,42 @@
+# 02 - Typealong React Admin Panel
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Conclusion](#conclusion)
+  - [Useful resources](#useful-resources)
+
+## Overview
+
+### Screenshot
+
+![screenshot1](./resources//screenshot/Screenshot01.PNG)
+![screenshot2](./resources//screenshot/Screenshot02.PNG)
+![screenshot3](./resources//screenshot/Screenshot03.PNG)
+![screenshot4](./resources//screenshot/Screenshot04.PNG)
+
+### Links
+
+- Live Site URL: [Click here](https://02-typealong-react-admin-panel-tutorial.netlify.app/)
+
+## My process
+
+### Built with
+
+- [React](https://reactjs.org/) - JS library
+- [React Router DOM](https://reactrouter.com/) - React routing library
+- [Material UI](https://mui.com/) - React Component library
+
+### Conclusion
+
+- Had a try at Material UI library and it ease up the development process.
+- React Router DOM is quite easy to use and learn.
+- Implemented dark mode using useContext at App component so that all components are able to access the state of dark mode.
+
+### Useful resources
+
+- Source Tutorial: [Click here](https://www.youtube.com/watch?v=yKV1IGahXqA)
---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,57 @@
+import './single.scss';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
+import Chart from '../../components/chart/Chart';
+import List from '../../components/table/Table';
+
+const Single = () => {
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemKey">janedoe@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemKey">+1 2321 12 12</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemKey">Elton St. 234 Garden City</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemKey">USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months )" />
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <List />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Single;
